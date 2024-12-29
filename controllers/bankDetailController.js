0ec46e58d81ee2c512a0b06080c062de4dec004e
Pramod <pramod52jpr@gmail.com>
2024-12-29 15:27:00 +0530

    my withdrawal feature

--- controllers/bankDetailController.js
+++ controllers/bankDetailController.js
@@ -75,6 +75,7 @@ exports.getAllWithdrawRequest = async (req, res) => {
         requests = requests.map(e => {
             return {...e._doc, bankDetails: bank.filter(ele => ele.userId.toString() == e.userId._id.toString())[0]};
         })
+        if(requests.length == 0) return res.send({status: false, message: 'No data found'});
         res.send({status: true, message: "Data fetched successfully", data: requests});
     }catch(e){
         res.send({status: false, message: e.message});
@@ -88,6 +89,21 @@ exports.sendMoneySuccess = async (req, res) => {
         withdrawData.success = true;
         await withdrawData.save();
         res.send({status: true, message: "Withdraw Status Updated"});
+    }catch(e){
+        res.send({status: false, message: e.message});
+    }
+}
+
+exports.getMyWithdrawals = async (req, res) => {
+    try{
+        const userId = req.user.id;
+        let requests = await Withdraw.find({userId}).populate('userId');
+        const bank = await BankDetail.find();
+        requests = requests.map(e => {
+            return {...e._doc, bankDetails: bank.filter(ele => ele.userId.toString() == e.userId._id.toString())[0]};
+        })
+        if(requests.length == 0) return res.send({status: false, message: 'No data found'});
+        res.send({status: true, message: "Data fetched successfully", data: requests});
     }catch(e){
         res.send({status: false, message: e.message});
     }
--- routes/bankDetailRoutes.js
+++ routes/bankDetailRoutes.js
@@ -4,7 +4,7 @@ const adminAuthVerify = require("../middlewares/adminAuth");
 
 const bankDetailRouter = express.Router();
 
-const {getBankDetail, updateBankDetail, getAllBankDetails, addWithdrawRequest, getAllWithdrawRequest, sendMoneySuccess} = require("../controllers/bankDetailController");
+const {getBankDetail, updateBankDetail, getAllBankDetails, addWithdrawRequest, getAllWithdrawRequest, sendMoneySuccess, getMyWithdrawals} = require("../controllers/bankDetailController");
 
 bankDetailRouter.get('/get_bank_details', authVerify, getBankDetail);
 bankDetailRouter.post('/update_bank_details', authVerify, updateBankDetail);
@@ -13,5 +13,6 @@ bankDetailRouter.get('/get_all_bank_details', adminAuthVerify, getAllBankDetails
 bankDetailRouter.post('/add_withdraw_request', authVerify, addWithdrawRequest);
 bankDetailRouter.get('/get_all_withdraw_request', adminAuthVerify, getAllWithdrawRequest);
 bankDetailRouter.post('/send_money_success', adminAuthVerify, sendMoneySuccess);
+bankDetailRouter.get('/get_my_withdrawals', authVerify, getMyWithdrawals);
 
 module.exports = bankDetailRouter;
